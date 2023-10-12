f0a2280e1ca35cda847e92bab2c0eddb4b6931fa
iGoodw1n <121541489+iGoodw1n@users.noreply.github.com>
2023-10-12 15:37:59 +0400

    Add project files.

---
+++ MathGame-CSA/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MathGame_CSA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
